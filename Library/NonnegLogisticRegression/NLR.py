e76c8dc31b55ed4d4dd9833b4b0b75410cfb19a2
Hoang Duong <duongtronghoang@gmail.com>
2015-03-26 22:28:21 -0700

    Fail attempt to write NonNegative Logistic Regression by SGD

--- Library/NonnegLogisticRegression/NLR.py
+++ Library/NonnegLogisticRegression/NLR.py
@@ -9,44 +9,30 @@ logger = logging.getLogger(__name__)
 
 
 def _multinomial_loss(w, X, Y, alpha):
-    sample_weight = np.ones(len(Y))
-    n_classes = Y.shape[1]
-    n_features = X.shape[1]
-    fit_intercept = w.size == (n_classes * (n_features + 1))
-    w = w.reshape(n_classes, -1)
-    sample_weight = sample_weight[:, np.newaxis]
-    if fit_intercept:
-        intercept = w[:, -1]
-        w = w[:, :-1]
-    else:
-        intercept = 0
+    w = w.reshape(Y.shape[1], -1)
     p = safe_sparse_dot(X, w.T)
-    p += intercept
     p -= logsumexp(p, axis=1)[:, np.newaxis]
-    loss = -(sample_weight * Y * p).sum()
+    loss = -(Y * p).sum()
     loss += 0.5 * alpha * squared_norm(w)
     p = np.exp(p, p)
-    return loss, p, w
+    return loss/len(Y), p, w
 
 def _multinomial_loss_grad(w, X, Y, alpha):
     sample_weight = np.ones(len(Y)) 
     n_classes = Y.shape[1]
     n_features = X.shape[1]
-    fit_intercept = (w.size == n_classes * (n_features + 1))
-    grad = np.zeros((n_classes, n_features + bool(fit_intercept)))
+    grad = np.zeros((n_classes, n_features))
     loss, p, w = _multinomial_loss(w, X, Y, alpha)
-    sample_weight = sample_weight[:, np.newaxis]
-    diff = sample_weight * (p - Y)
-    grad[:, :n_features] = safe_sparse_dot(diff.T, X)
-    grad[:, :n_features] += alpha * w
-    if fit_intercept:
-                grad[:, -1] = diff.sum(axis=0)
-    return loss, grad, p
+    diff = p - Y
+    grad = safe_sparse_dot(diff.T, X)
+    grad += alpha * w
+    return loss, grad/len(Y), p
+
+
 
 def _batch_gen(data, batch_size):
     for i in xrange(0, len(data) - batch_size, batch_size):
         yield data[i:(i+batch_size)]
-
 
 def SetPositive(w):
     I = np.identity(9)
@@ -90,8 +76,6 @@ def _sgd(X, Y, alpha, max_epoch = 10, batch_size = 128,
             w_old, grad_old = w, grad
             w = w_old - t*grad_old
             grad_old = grad
-            w = SetPositive(w)
-            #if loss < eps: break
             train_loss, _, _ = _multinomial_loss(w, X[train], Y[train], alpha)
             valid_loss, _, _ = _multinomial_loss(w, X[valid], Y[valid], alpha)
             logger.info("%10g | %10g", train_loss/len(train), 
@@ -99,10 +83,12 @@ def _sgd(X, Y, alpha, max_epoch = 10, batch_size = 128,
     return w
 
 
-def _sgd2(X, Y, alpha, step_size = .001, max_epoch = 10, batch_size = 128, 
-          validation = .1, random_state = None):
+def SGD(X, Y, alpha, w_initial = None, step_size = .001, max_epoch = 10, 
+        batch_size = 8192, validation = .1, random_state = None):
     np.random.seed(random_state)
-    w = np.random.randn(Y.shape[1], X.shape[1])
+    if w_initial is None:
+        w = np.random.randn(Y.shape[1], X.shape[1])
+    else: w = w_initial
     idx = np.arange(len(Y))
     np.random.shuffle(idx)
     if type(validation) is float:
@@ -122,12 +108,9 @@ def _sgd2(X, Y, alpha, step_size = .001, max_epoch = 10, batch_size = 128,
         for batch in _batch_gen(train, batch_size):
             _, grad, _ = _multinomial_loss_grad(w, X[batch], Y[batch], alpha)
             w = w - step_size*grad
-            w = SetPositive(w)
-            loss, _, _ = _multinomial_loss(w, X, Y, alpha)
-            train_loss, _, _ = _multinomial_loss(w, X[train], Y[train], alpha)
-            valid_loss, _, _ = _multinomial_loss(w, X[valid], Y[valid], alpha)
-            logger.info("%10g | %10g", train_loss/len(train),
-                                       valid_loss/len(valid))
+        train_loss, _, _ = _multinomial_loss(w, X[train], Y[train], alpha)
+        valid_loss, _, _ = _multinomial_loss(w, X[valid], Y[valid], alpha)
+        logger.info("%10g | %10g", train_loss, valid_loss)
     return w
 
 if False:
@@ -141,17 +124,20 @@ if False:
     prob = np.exp(log_odd)/(1 + np.exp(log_odd))
     y = np.array([np.argmax(i) for i in prob])
     lb = LabelBinarizer()
-    y = lb.fit_transform(y)
-    res1 =  _sgd(X, y, .1, max_epoch = 20, validation = .1, 
-        batch_size = 1000, random_state = 1) 
-    res2 = _sgd2(X, y, .1, step_size = 1./len(X), max_epoch = 20,
-        batch_size = 1000, random_state = 1)
+    Y = lb.fit_transform(y)
+    train = np.arange(n_obs/2); valid = np.arange(n_obs/2,n_obs)
+    res = SGD(X[train], Y[train], 
+                    alpha = .1,      step_size = 2, 
+                max_epoch = 20,     batch_size = 1000,
+                w_initial = None, random_state = 1)
+    _, p, _ = _multinomial_loss(res, X[valid], Y[valid], .1) 
+    lr = LogisticRegression(C = .1, fit_intercept = False)
+    lr.fit(X[train], y[train])
+    probs = lr.predict_proba(X[valid])
 
-    _, y, _ = LoadData(); del _
-    X1, X1test = GetDataset('L1_btc_svc_mpc')
-    y = lb.fit_transform(y)
-    I = np.identity(9)
-    O = np.zeros((9,9))
-    I1 = np.hstack([I, O, O])
-    _multinomial_loss(I1, X1, y, 0)[0]/len(y)
-
+    ###
+    Y = lb.fit_transform(y)
+    w = SGD(X[train], Y[train], 
+                    alpha = .1,      step_size = .1,
+                max_epoch = 2 ,     batch_size = 4000,
+                w_initial = w,    random_state = 1)
